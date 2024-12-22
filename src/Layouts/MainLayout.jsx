3692addb02fbb6358492898873b797e07629c1ba
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-12-22 14:39:01 +0600

    working with banner

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
-import Navbar from "../Pages/Home/Navbar";
+import Navbar from "../Components/Navbar";
 
 const MainLayout = () => {
   return (
--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router";
 import MainLayout from "../Layouts/MainLayout";
+import Banner from "../Pages/Home/Banner";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<MainLayout />} />
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Banner />} />
+      </Route>
     </Routes>
   );
 };
