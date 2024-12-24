4f278a9f51244faa1afeaf501bafb1b7f80b05aa
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-12-24 14:39:45 +0600

    banner image added

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,33 +1,38 @@
+import image from "../../assets/pictures/banner.png";
+
 const Banner = () => {
   return (
-    <div
-      className="hero bg-fixed bg-center bg-cover min-h-screen lg:min-h-[80vh]"
-      style={{
-        backgroundImage:
-          "url(https://t4.ftcdn.net/jpg/05/90/17/23/360_F_590172388_eEZ8vGqNqAtYSNP7LcRTMyl0db5kemNX.jpg)",
-      }}
-    >
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="container mx-auto">
-        <div className="">
-          <div className="text-white">
-            <h3 className="text-2xl text-white font-light mb-4">
-              Namaste, Welcome to Amrutam
-            </h3>
-            <h1 className="mb-5 text-2xl lg:text-5xl font-bold">
-              Step into Holistic Healing with{" "}
-              <span className="underline">Ayurveda</span> <br /> Book
-              Consultation with certified Experts.
-            </h1>
-            <p className="mb-5 max-w-lg">
-              Dive into the world of ayurveda and Experience Personalized Health
-              Solutions and Holistic Guidance from Trusted Ayurvedic Doctors
-              Anytime, anywhere.
-            </p>
-            <button className="btn uppercase bg-secondary text-white border-none hover:text-secondary">
-              Book An Appointment
-            </button>
-          </div>
+    <div className="hero min-h-screen lg:min-h-[80vh] relative">
+      {/* Gradient Overlay for Left Half */}
+      <div className="absolute inset-0 bg-gradient-to-r from-black via-black/95 to-black/30 z-10"></div>
+
+      {/* Right Half Background Image */}
+      <div
+        className="absolute top-0 right-0 h-full w-1/2 bg-cover bg-right"
+        style={{
+          backgroundImage: `url(${image})`,
+        }}
+      ></div>
+
+      {/* Content */}
+      <div className="container mx-auto relative z-20">
+        <div className="text-white max-w-lg">
+          <h3 className="text-2xl text-white font-light mb-4">
+            Namaste, Welcome to Amrutam
+          </h3>
+          <h1 className="mb-5 text-2xl lg:text-5xl lg:min-w-[80vw] font-bold">
+            Step into Holistic Healing with{" "}
+            <span className="underline">Ayurveda</span> <br /> Book Consultation
+            with certified Experts.
+          </h1>
+          <p className="mb-5">
+            Dive into the world of ayurveda and Experience Personalized Health
+            Solutions and Holistic Guidance from Trusted Ayurvedic Doctors
+            Anytime, anywhere.
+          </p>
+          <button className="btn uppercase bg-secondary text-white border-none hover:text-secondary">
+            Book An Appointment
+          </button>
         </div>
       </div>
     </div>
